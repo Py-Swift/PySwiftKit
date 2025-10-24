831dd1a1e2b16a9bfae4380aa37269000e4ab532
Py-Swift <107263226+Py-Swift@users.noreply.github.com>
2025-10-24 21:40:08 +0200

    Update PyDeserialize+Strings.swift

--- Sources/PySerializing/PyDeserialize/PyDeserialize+Strings.swift
+++ Sources/PySerializing/PyDeserialize/PyDeserialize+Strings.swift
@@ -5,3 +5,15 @@
 
 import CPython
 import PySwiftKit
+
+
+extension String: PyDeserialize {
+    public static func casted(from object: PyPointer) throws -> String {
+        guard PyObject_TypeCheck(object, .PyUnicode) else { throw PyStandardException.unicodeError }
+        return .init(cString: PyUnicode_AsUTF8(object))
+    }
+    
+    public static func casted(unsafe object: PyPointer) throws -> String {
+        .init(cString: PyUnicode_AsUTF8(object))
+    }
+}
