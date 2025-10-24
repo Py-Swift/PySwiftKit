91cc2ba8c9b9f68b7309eb6e5d425960f0976638
Py-Swift <107263226+Py-Swift@users.noreply.github.com>
2025-10-24 21:33:40 +0200

    Add PyDeserialize conformance for Set type
    Implements the PyDeserialize protocol for Set where Element conforms to PyDeserialize. Adds methods to cast Python set objects to Swift Set<Element> using both safe and unsafe casting.

--- Sources/PySerializing/PyDeserialize/PyDeserialize+Set.swift
+++ Sources/PySerializing/PyDeserialize/PyDeserialize+Set.swift
@@ -5,3 +5,27 @@
 
 import CPython
 import PySwiftKit
+
+
+extension Set: PyDeserialize where Element: PyDeserialize {
+    public static func casted(from object: PyPointer) throws -> Set<Element> {
+        guard PyObject_TypeCheck(object, .PySet) else { throw PyStandardException.typeError }
+        let size = PySet_Size(object)
+        let iter = PyObject_GetIter(object)
+        var set = Set<Element>()
+        while let next = PyIter_Next(iter) {
+            set.insert(try .casted(from: next))
+        }
+        return set
+    }
+    
+    public static func casted(unsafe object: PyPointer) throws -> Set<Element> {
+        let size = PySet_Size(object)
+        let iter = PyObject_GetIter(object)
+        var set = Set<Element>()
+        while let next = PyIter_Next(iter) {
+            set.insert(try .casted(from: next))
+        }
+        return set
+    }
+}
