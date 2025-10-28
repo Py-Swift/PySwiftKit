e3050f7df9c5c0123dfc86c3831cbe5c5b65dd4b
Py-Swift <psychowaspx@gmail.com>
2025-10-28 08:28:09 +0100

    Update PyDeserialize+FloatingPoint.swift

--- Sources/PySerializing/PyDeserialize/PyDeserialize+FloatingPoint.swift
+++ Sources/PySerializing/PyDeserialize/PyDeserialize+FloatingPoint.swift
@@ -34,3 +34,18 @@ extension Float32: PyDeserialize {}
 @available(iOS 16,macOS 11, *)
 extension Float16: PyDeserialize {}
 #endif
+
+
+#if canImport(CoreFoundation)
+import CoreFoundation
+extension CGFloat: PyDeserialize {
+    public static func casted(unsafe object: PyPointer) throws -> Self {
+        PyFloat_AS_DOUBLE(object)
+    }
+    
+    public static func casted(from object: PyPointer) throws -> Self {
+        guard PyObject_TypeCheck(object, .PyFloat) else { throw PyStandardException.typeError }
+        return PyFloat_AS_DOUBLE(object)
+    }
+}
+#endif
