4e3512944e8f17982a0a582ad150f68083752e1a
Py-Swift <psychowaspx@gmail.com>
2025-11-03 01:10:04 +0100

    Add casted methods for Dictionary with PyPointer values
    Introduces two static methods, casted(from:) and casted(unsafe:), to convert PyPointer objects to Dictionary<Key, PyPointer> where Key conforms to PyDeserialize. These methods facilitate safe and unsafe deserialization of Python dictionaries into Swift dictionaries.

--- Sources/PySerializing/PyDeserialize/PyDeserialize+Dictionary.swift
+++ Sources/PySerializing/PyDeserialize/PyDeserialize+Dictionary.swift
@@ -36,3 +36,34 @@ extension Dictionary: PyDeserialize where Key: PyDeserialize,  Value: PyDeserial
     
     
 }
+
+
+extension Dictionary where Key: PyDeserialize,  Value == PyPointer {
+    
+    public static func casted(from object: PyPointer) throws -> Dictionary<Key, Value> {
+        guard PyObject_TypeCheck(object, .PyDict) else { throw PyStandardException.typeError }
+        var d: [Key:Value] = .init()
+        var pos: Int = 0
+        var key: PyPointer?
+        var value: PyPointer?
+        while PyDict_Next(object, &pos, &key, &value) == 1 {
+            guard let key, let value else { throw PyStandardException.keyError }
+            d[try Key.casted(from: key)] = value
+        }
+        return d
+    }
+    
+    public static func casted(unsafe object: PyPointer) throws -> Dictionary<Key, Value> {
+        var d: [Key:Value] = .init()
+        var pos: Int = 0
+        var key: PyPointer?
+        var value: PyPointer?
+        while PyDict_Next(object, &pos, &key, &value) == 1 {
+            guard let key, let value else { throw PyStandardException.keyError }
+            d[try Key.casted(unsafe: key)] = value
+        }
+        return d
+    }
+    
+    
+}
