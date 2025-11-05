67e4fe1dabf7cb21b2cea9283569c7873907ca13
Py-Swift <psychowaspx@gmail.com>
2025-11-05 21:09:03 +0100

    Comment out 'inline' for PyObject_AS_SwiftPtr
    The 'inline' keyword was commented out in both the C and header files for the PyObject_AS_SwiftPtr function. This may address compatibility or linkage issues with the function's usage.

--- Sources/CPySwiftObject/CPySwiftObject.h
+++ Sources/CPySwiftObject/CPySwiftObject.h
@@ -28,7 +28,7 @@ PySwiftObject* _PySwiftObject_Create(PyTypeObject *type, void *swift_object);
 PyObject* _PySwiftObject_New(PyTypeObject *type);
 PySwiftObject* PySwiftObject_Cast(PyObject* o);
 
-inline
+//inline
 void* PyObject_AS_SwiftPtr(PyObject* o);
 
 
